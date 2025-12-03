d6c75fc8b15b3971953f2988733bdc8e81465b2c
Bartosz Bróździński <01187212@pw.edu.pl>
2025-12-03 23:30:02 +0900

    added mongodb connection

--- backend/app/src/main/main.py
+++ backend/app/src/main/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import pymongo
 
+# creating api
 app = FastAPI()
 
+# connecting to db
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+database = client["mongodb_local"]
+print(database.list_collection_names())
+print(client.server_info())
 
+
+# endpoints
 @app.get("/")
 def root():
     return {"huj": "ci w dupe"}
