4ab3ced703b4ed9514cfb2b2913db324cbb0969b
kblaszcz <01187210@pw.edu.pl>
2025-12-08 22:48:28 +0900

    idk

--- moodify/app/components/AddToProfileButton.tsx
+++ moodify/app/components/AddToProfileButton.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 export default function AddToProfileButton({ movieId }: { movieId: string }) {
-  const handleClick = () => {
-    console.log("Adding movie:", movieId);
-    alert("Movie added to your profile! (mock)");
+  const handleClick = async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/users/add_movie/${movieId}`, {
+        method: "POST",
+        credentials: "include",
+        });
+
+
+      if (!res.ok) {
+        const err = await res.json().catch(() => ({}));
+        alert("Error: " + (err.detail || "cannot add movie"));
+        return;
+      }
+
+      alert("Movie added!");
+    } catch (err) {
+      console.error(err);
+      alert("Request failed");
+    }
   };
 
   return (
