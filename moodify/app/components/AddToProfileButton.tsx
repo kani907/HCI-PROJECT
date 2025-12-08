fc95d7ade58864b79a7d7b634ff9708f3692f043
kblaszcz <01187210@pw.edu.pl>
2025-12-08 22:59:33 +0900

    working adding movie

--- moodify/app/components/AddToProfileButton.tsx
+++ moodify/app/components/AddToProfileButton.tsx
@@ -3,11 +3,24 @@
 export default function AddToProfileButton({ movieId }: { movieId: string }) {
   const handleClick = async () => {
     try {
+      // get JWT token stored after login
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        alert("You are not logged in");
+        return;
+      }
+
       const res = await fetch(`http://localhost:8000/users/add_movie/${movieId}`, {
         method: "POST",
         credentials: "include",
-        });
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
 
+      console.log(res);
 
       if (!res.ok) {
         const err = await res.json().catch(() => ({}));
